03da50acc878d62f4810fc7c6ea7e2712e56a6b5
Shuvo <shovocse753@gmail.com>
2020-08-15 15:43:58 +0600

    Due items view is changed.

--- app/src/main/java/org/binaryitplanet/tradinget/Features/Adapter/DueDateAdapter.kt
+++ app/src/main/java/org/binaryitplanet/tradinget/Features/Adapter/DueDateAdapter.kt
@@ -1,12 +1,21 @@
 package org.binaryitplanet.tradinget.Features.Adapter
 
+import android.Manifest
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.view_due_dates_list.view.*
 import org.binaryitplanet.tradinget.R
+import org.binaryitplanet.tradinget.Utils.Config
 import org.binaryitplanet.tradinget.Utils.LedgerUtils
 
 class DueDateAdapter(
@@ -33,10 +42,27 @@ class DueDateAdapter(
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         var view = holder.itemView
 
-        view.ledgerId.text = "ID: " + ledgerList[position].ledgerId
+        view.buyerName.text = "Buyer name: " + ledgerList[position].stakeHolderName
 
-        view.dueDate.text = "Due date: " + ledgerList[position].dueDate
+        view.dueAmount.text = "Due amount: ${Config.RUPEE_SIGN} " +
+                (ledgerList[position].totalAmount - ledgerList[position].paidAmount)
+
+        view.call.setOnClickListener {
+            makeCall(ledgerList[position].mobileNumber)
+        }
     }
 
+    private fun makeCall(number:String) {
+        Log.d(TAG, "Making call to $number")
+        val intent = Intent(Intent.ACTION_CALL)
+        intent.data = Uri.parse("tel:$number")
+        if(ContextCompat.checkSelfPermission(context,
+                Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(context as Activity,
+                arrayOf(Manifest.permission.CALL_PHONE), Config.REQUEST_CALL)
+        } else {
+            context.startActivity(intent)
+        }
+    }
     inner class ViewHolder(view: View): RecyclerView.ViewHolder(view) {}
 }
